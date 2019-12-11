10283fdde74b5adc26244a312e380ea35dd9fda7
zhangle <zhangle@dangdang.com>
2019-12-11 15:36:42 +0800

    update

--- demo/api/FlaskDemo.py
+++ demo/api/FlaskDemo.py
@@ -1,11 +1,21 @@
 from flask import Flask, request, json
+import pymysql
 
 app = Flask(__name__)
 
-
 @app.route('/helloWorld')
 def hello_world():
-    return 'Hello World!'
+    conn = pymysql.connect(host='10.255.242.136', port=3306, user='kefu_ai_user', passwd='kefu_ai_dev', db='kefu_ai')
+    # 创建游标
+    cursor = conn.cursor()
+    # 执行SQL，并返受影响行数
+    effect_row = cursor.execute("SELECT * FROM question_classify_train order by id desc limit 10")
+    print(effect_row)
+    row_2 = cursor.fetchall()
+    # 关闭游标
+    cursor.close()
+    conn.close()
+    return str(row_2)
 
 
 @app.route('/helloWorldJson', methods=['POST'])
@@ -14,9 +24,9 @@ def hello_world_json():
         data = request.get_data()
         print(data)
         json_data = json.loads(data.decode("utf-8"))
-        print(json_data);
-        return data;
+        print(json_data)
+        return data
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8888, debug=False);
+    app.run(host='127.0.0.1', port=8888, debug=False)
--- demo/common/httpTest.py
+++ demo/common/httpTest.py
@@ -1,9 +1,71 @@
-#import nltk
 import requests
 import json
+import time
+import re
 
-url = 'http://api.dangdang.com/shopim/user/getCustInfo?tenantId=1&custId=30236317'
+#url = 'http://api.dangdang.com/shopim/user/getCustInfo?tenantId=1&custId=30236317'
+#r = requests.get(url)
+#result = json.loads(r.text)
+# print(result)
 
-r = requests.get(url)
-result = json.loads(r.text)
-print(result)
+
+#posturl = 'http://10.7.41.191:9009/msgcenter/qualityCheck/querySessionForQc'
+headers = {'Content-Type': 'application/json;charset=UTF-8'}
+#a = {"tenantId":1,"startTime":1562342400000,"endTime":1565020799000,"pageNum":1,"pageSize":10,"shopId":"0"}
+#res = requests.post(posturl,json=a,headers=headers)
+# print(res.text)
+
+def getSession(roomId, sessionId):
+    try:
+        posturl = 'http://10.7.41.191:9009/msgcenter/session/querySessionDetail'
+        b = {"tenantId": 1, "pageNum": 1, "pageSize": 1, "roomId": roomId, "sessionId": sessionId}
+        res = requests.post(posturl, json=b, headers=headers)
+        #print(res.text)
+        j = json.loads(res.text)
+        if (j['data']['session']['isTransManual'] == 1 and j['data']['session']['shopId'] == "0"):
+            return j['data']['session']['transManualStartDate']
+        #print(transDate)
+        return None
+    except Exception as error:
+        print("error:" + roomId, error)
+
+
+def getMessage(roomId, sessionId, transDate):
+    msgurl = 'http://10.7.41.191:9009/msgcenter/message/queryBySession'
+    p = {"tenantId": 1, "pageNum": 1, "pageSize": 100, "roomId": roomId, "sessionId": sessionId}
+    msgres = requests.post(msgurl, json=p, headers=headers)
+    msgList = json.loads(msgres.text)['data']['list']
+    cList = []
+    for item in msgList:
+        date_time = time.strptime(item['sendTime'], "%Y-%m-%d %H:%M:%S")
+        if (transDate == None or time.mktime(date_time) < transDate/1000) and item['senderType'] == 1 and item['contentType'] == 1:
+            cList.append(item['content'])
+    if len(cList) == 0:
+        return None
+    l = cList[:3]
+    l.reverse()
+    return ",".join(i for i in l)
+
+def invokeAi(roomId,custId,sessionId,content):
+    aiurl = 'http://10.255.242.225:9000/classify/get'
+    p = {"tenantId": 1, "roomId": roomId, "custId": custId, "sessionId": sessionId, "companyId": 1,"content": content}
+    aires = requests.post(aiurl, json=p, headers=headers)
+    print(aires.text)
+
+def main():
+    txt=open("H:\\分类语料.txt", 'r', encoding='utf8')
+    for line in txt:
+        line = re.sub(r'[^A-Za-z0-9\t]+',' ',line)
+        s = line.split('\t')
+        roomId = s[0]
+        sessionId = s[1]
+        custId = s[2]
+        tDate = getSession(roomId, sessionId)
+        content = getMessage(roomId, sessionId, tDate)
+        if (content and len(content) > 3):
+            invokeAi(roomId,custId,sessionId,content)
+            print(content)
+    txt.close()
+
+if __name__ == '__main__':
+    main()
